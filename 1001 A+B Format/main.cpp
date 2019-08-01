461446f4dd10addfba94c875ccfeba9a744ab29f
zideliu <34944964+zideliu@users.noreply.github.com>
2019-08-01 17:01:32 +0800

    Add files via upload

---
+++ 1001 A+B Format/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  1001 A+B Format
+//
+//  Created by Zide Liu on 2019/8/1.
+//  Copyright © 2019 Zide Liu. All rights reserved.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int a, b, c;
+    string s;
+    cin>>a>>b;
+    c = a + b;
+    s = to_string(c);// to_string 将数值转化为字符串。返回对应的字符串。
+    unsigned long n = s.size();
+    int temp = 0;
+    if (c < 0) temp = 1;
+    for (unsigned long i = n - 3; i > temp; i -= 3) {
+        s.insert(i, ",");// s.insert(pos, str)，pos是待插入的位置， str是待插入的字符串
+    }
+    cout << s << endl;
+    return 0;
+}
