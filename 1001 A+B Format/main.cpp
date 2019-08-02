6be4bcc9cc168f8967aa69e2bfcbd2de82bc0e10
zideliu <34944964+zideliu@users.noreply.github.com>
2019-08-02 15:06:32 +0800

    Update main.cpp

--- 1001 A+B Format/main.cpp
+++ 1001 A+B Format/main.cpp
@@ -9,6 +9,7 @@
 #include <string>
 using namespace std;
 int main() {
+//第一种方法
     int a, b, c;
     string s;
     cin>>a>>b;
@@ -21,5 +22,28 @@ int main() {
         s.insert(i, ",");// s.insert(pos, str)，pos是待插入的位置， str是待插入的字符串
     }
     cout << s << endl;
+//第二种方法
+    int k[8],i=0,flag=0;
+    if(c<0){
+        c=-c;
+        flag=1;
+    }
+    if(c==0)cout<<"0";
+    else{
+        while(c)
+        {
+            k[7-i]=c%10;
+            c=c/10;
+            i++;
+        }
+        if(flag)cout<<"-";
+        for(i=8-i;i<8;i++)
+        {
+            cout<<k[i];
+            if(i==1||i==4)
+                cout<<",";
+        }
+    }
     return 0;
 }
+
